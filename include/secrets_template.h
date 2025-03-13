86b796f239ef6674298fce05b666e1b310d9698a
GCinini <gc@pm.me>
2025-03-13 10:29:28 -0300

    Created the secrets.h and template, added secrets.h to gitignore

---
+++ include/secrets_template.h
@@ -0,0 +1,8 @@
+// secrets_template.h - Rename this to secrets.h and add your own credentials
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
+
+#endif // SECRETS_H
